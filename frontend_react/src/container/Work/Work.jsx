2e55c9086753cb3093bb012c7d80922fc6344037
sophonTBP <lucas.jumelin@live.fr>
2022-11-18 03:06:18 +0100

    test

--- frontend_react/src/container/Work/Work.jsx
+++ frontend_react/src/container/Work/Work.jsx
@@ -70,7 +70,7 @@ const Work = () => {
                 className="app__work-hover app__flex"
               >
 
-                <a href='https://earnest-moonbeam-19d155.netlify.app/' target="_blank" rel="noreferrer">
+                <a href='https://gilded-moonbeam-9e1b13.netlify.app/' target="_blank" rel="noreferrer">
                   <motion.div
                     whileInView={{ scale: [0, 1] }}
                     whileHover={{ scale: [1, 0.90] }}
@@ -89,16 +89,36 @@ const Work = () => {
                   transition={{ duration: 0.25 }}
                   className="app__flex"
                 >
-                  <AiFillGithub  />
+                  <AiFillGithub />
                 </motion.div>
 
               </motion.div>
             </div>
 
             <div className="app__work-content app__flex">
-              <h2 className="p-text">{work.title}</h2>
-              <h1 className="bold-text" style={{ marginTop: 10 }}>{work.description}</h1>
-              <p className="bold-text" style={{ marginTop: 10 }}>{work.texte}</p>
+              <h2 className="bold-text workTitle">{work.title}</h2>
+              <h1 className="bold-text workDescription" style={{ marginTop: 10 }}>{work.description}</h1>
+              <p className="bold-text" style={{ marginTop: 15 }}>
+                Solution complete pour site e-commerce, composée de:</p>
+              <p className="bold-text" style={{ marginTop: 15 }}>
+                Un site vitrine sécurisé avec login et moyen de paiement intégré. Le caddie est persistant que l'utilisateur soit enregistré ou non. Si l'utilisateur est enregistré son caddie est accésible depuis n'importe quel appareil connecté a son compte.
+                Le site s'adapte à toutes les tailles d'écrans et les logos, bouttons et le fond d'écran sont costumisable par nos soins. 
+                Enfin la vitrine est optimisée pour charger rapidement se qui améliore le référencement google et permet d'apparaître dans les premiers résultats du moteur de recherche. </p>
+
+              <p className="bold-text" style={{ marginTop: 15 }}>
+                Un studio pour gérer l'achalandage: non référencé par google et protégé par un mot de passe administrateur on l'utilisé pour définir la bannière (pour mettre en avant un produit), les noms des produits, leurs prix, leurs fiche d'information et c'est ici que l'on upload les vignettes pour la vitrine.
+              </p>
+
+              <p className="bold-text" style={{ marginTop: 10 }}>
+                Un gestionaire de Commande : lui aussi non référencé par google et protégé par un mot de passe administrateur il permet au gestionaire des stocks de préparer les colis. Une fois la commande prète à être prise en charge par le transporteur il sufit de rentrer le numero de suivi pour que le client le reçoive automatiquement par mail. Les donées utilisateurs ne sont concervé sur le site que le temps que la commande soit envoyée, seuls les numeros de commandes et de transactions sont conservés.
+              </p>
+
+              <p className="bold-text" style={{ marginTop: 10 }}>
+                *note: la gestion des données personelles des utilisateurs étant un domaine trés sensible leur sauvegarde est assurée par Stripes, en particulier les numeros de carte de crédits ne transitent jamais sur nos serveurs (vous recevez les paiement directement sur votre compte bancaire) cependant en cas de besoin il est trés facile de consulter l'adresse et le nom du client en utilisant le numero de transaction sur votre compte stripes que nous configurerons avec vous`
+              </p>
+
+
+
               {/* <div className="app__work-tag app__flex">
                 <p className="p-text">{work.tags[0]}</p>
               </div>  */}
